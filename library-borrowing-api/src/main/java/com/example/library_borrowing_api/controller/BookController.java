0ff453f7e9f0bfaa9737d088f361906006593841
stevenkarta5-bit <stevenkarta5@gmail.com>
2025-09-10 13:14:54 +0700

    add ControllerBook

--- library-borrowing-api/src/main/java/com/example/library_borrowing_api/controller/BookController.java
+++ library-borrowing-api/src/main/java/com/example/library_borrowing_api/controller/BookController.java
@@ -1,4 +1,57 @@
 package com.example.library_borrowing_api.controller;
 
+import com.example.library_borrowing_api.dto.book.BookResponse;
+import com.example.library_borrowing_api.dto.book.CreateBookRequest;
+import com.example.library_borrowing_api.entity.BookEntity;
+import com.example.library_borrowing_api.repository.BookRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+    public final BookRepository bookRepository;
+
+    public BookController (BookRepository bookRepository){
+
+        this.bookRepository = bookRepository;
+
+    }
+    @PostMapping
+    public ResponseEntity <BookResponse> createBook (@Valid @RequestBody CreateBookRequest create) {
+        BookEntity book = BookEntity.builder()
+                .bookName(create.getBookName())
+                .isbn(create.getIsbn())
+                .build();
+        bookRepository.save(book);
+
+        BookResponse response = BookResponse.builder()
+                .id(book.getId())
+                .bookName(book.getBookName())
+                .isbn(book.getIsbn())
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookResponse>> getAllMembers() {
+        List<BookEntity> books = bookRepository.findAll();
+
+        List<BookResponse> responseAll = books.stream()
+                .map(book -> BookResponse.builder()
+                        .id(book.getId())
+                        .bookName(book.getBookName())
+                        .isbn(book.getIsbn())
+                        .build()
+                )
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(responseAll);
+    }
+
+    @GetMapping
+
 }
