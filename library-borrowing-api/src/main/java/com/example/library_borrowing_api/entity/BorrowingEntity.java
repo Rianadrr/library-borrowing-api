46bad3ef748b4c306624e836a3a481deae6c302c
Rian Adriansyah <rian.adriansyah@fifgroup.astra.co.id>
2025-09-10 09:32:17 +0700

    add member and borrowing entity

--- library-borrowing-api/src/main/java/com/example/library_borrowing_api/entity/BorrowingEntity.java
+++ library-borrowing-api/src/main/java/com/example/library_borrowing_api/entity/BorrowingEntity.java
@@ -1,4 +1,36 @@
 package com.example.library_borrowing_api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "borrowings")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BorrowingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long borrowId;
+
+    @ManyToMany
+    private BookEntity;
+
+    @ManyToMany
+    private MemberEntity;
+
+    @Column(nullable = false)
+    private LocalDate borrowDate;
+
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private String status;
 }
