0818eab8f688f64cede2396fe98c8649970c0ce5
stevenkarta5-bit <stevenkarta5@gmail.com>
2025-09-10 10:02:16 +0700

    add bookRepository

--- library-borrowing-api/src/main/java/com/example/library_borrowing_api/repository/BookRepository.java
+++ library-borrowing-api/src/main/java/com/example/library_borrowing_api/repository/BookRepository.java
@@ -1,4 +1,11 @@
 package com.example.library_borrowing_api.repository;
 
-public interface BookRepository {
+import com.example.library_borrowing_api.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface BookRepository extends JpaRepository<BookEntity , Long> {
+
 }
